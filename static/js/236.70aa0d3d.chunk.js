4fbbc7a9dde43e4695d1e23d0f9288f860dad0ec
yulia-bisyuk <80700623+yulia-bisyuk@users.noreply.github.com>
2022-05-07 16:07:33 +0000

    Deploying to gh-pages from @ yulia-bisyuk/goit-react-hw-05-movies@9975f59e956da878d9c2ab1630fc2832d1edcacb 🚀

---
+++ static/js/236.70aa0d3d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[236],{3236:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var r,i=t(501),c=t(168),o=t(6031).ZP.p(r||(r=(0,c.Z)(["\n  color: #063970;\n"]))),s=t(184),u=function(){return(0,s.jsx)("div",{children:(0,s.jsxs)(o,{children:["We didn't find anything... Please return to"," ",(0,s.jsx)(i.rU,{to:"/",children:"Home Page"})]})})}}}]);
+//# sourceMappingURL=236.70aa0d3d.chunk.js.map
